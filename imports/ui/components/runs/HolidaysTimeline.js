c2b13e1754f6b75b77305b5d777629443ec4de2e
KyneSilverhide <kyne.silverhide@gmail.com>
2017-03-23 20:07:33 +0100

    Improved timeline to show dev days and holidays. Added version period. Added totla dev days each weeks

--- imports/ui/components/runs/HolidaysTimeline.js
+++ imports/ui/components/runs/HolidaysTimeline.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import Timeline from 'react-calendar-timeline';
 import moment from 'moment';
+import 'moment/locale/fr';
+import { extendMoment } from 'moment-range';
 
 export default class HolidaysTimeline extends React.Component {
+
+  constructor(props) {
+    super(props);
+    moment.locale('fr');
+  }
 
   getGroups() {
     const { run } = this.props;
@@ -24,20 +31,20 @@ export default class HolidaysTimeline extends React.Component {
     return groups;
   }
 
-  getVersionDevDateItem(run) {
+  getVersionDevelopmentPeriod(run) {
     return {
-      id: 0,
+      id: 'version_period',
       group: 0,
       title: 'Développement',
       start_time: run.version.startDate,
       end_time: run.version.freezeDate,
-      className: 'development',
+      className: 'version',
     };
   }
 
-  getVersionFreezeDateItem(run) {
+  getVersionFreezePeriod(run) {
     return {
-      id: 1,
+      id: 'freeze_period',
       group: 0,
       title: 'Freeze/Validation',
       start_time: run.version.freezeDate,
@@ -46,28 +53,120 @@ export default class HolidaysTimeline extends React.Component {
     };
   }
 
-  getDevHolidayDateItem(id, devHoliday) {
+  getLegalHolidayDateItem(developer, holiday) {
+    const id = `holiday_${developer._id}_${holiday.date}`;
+    return {
+      id,
+      group: developer._id,
+      title: holiday.name,
+      start_time: moment(holiday.date),
+      end_time: moment(holiday.date).endOf('day'),
+      className: 'holiday',
+    };
+  }
+
+  getHolidayDateItem(developer, devHoliday) {
+    const id = `devholiday_${developer._id}_${devHoliday.date}`;
     return {
       id,
       group: devHoliday.developerId,
-      title: devHoliday.halfDay ? '0.5' : '1',
+      title: devHoliday.halfDay ? developer.devRatio / 200 : 0,
       start_time: devHoliday.date,
       end_time: moment(devHoliday.date).endOf('day'),
       className: devHoliday.halfDay ? 'halfday' : 'fullday',
     };
   }
 
+  getDevDateItem(developer, day) {
+    const id = `devdate_${developer._id}_${day}`;
+    return {
+      id,
+      group: developer._id,
+      title: developer.devRatio / 100,
+      start_time: day,
+      end_time: moment(day).endOf('day'),
+      className: 'development',
+    };
+  }
+
+  findHoliday(day) {
+    const { run } = this.props;
+    for (const holiday of run.holidays) {
+      if (moment(holiday.date).isSame(day, 'day')) {
+        return holiday;
+      }
+    }
+    return null;
+  }
+
+  findDevHoliday(day, developer) {
+    const { run } = this.props;
+    for (const devHoliday of run.devHolidays) {
+      if (devHoliday.developerId === developer._id && devHoliday.date.isSame(day, 'day')) {
+        return devHoliday;
+      }
+    }
+    return null;
+  }
+
+  buildTimelineItemAt(developer, day) {
+    const legalHoliday = this.findHoliday(day);
+    if (legalHoliday != null) {
+      return this.getLegalHolidayDateItem(developer, legalHoliday);
+    }
+    const devHoliday = this.findDevHoliday(day, developer);
+    if (devHoliday != null) {
+      return this.getHolidayDateItem(developer, devHoliday);
+    }
+    return this.getDevDateItem(developer, day);
+  }
+
+  buildWeekItemBetween(startOfWeek, endOfWeek, actualDevDays, totalDevDays) {
+    return {
+      id: `week_${startOfWeek}`,
+      group: -1,
+      title: `${actualDevDays} jours (${totalDevDays})`,
+      start_time: startOfWeek,
+      end_time: endOfWeek,
+      className: 'total-week',
+    };
+  }
+
+  isNumeric(num) {
+    return !isNaN(num);
+  }
+
   getDateItems() {
+    const exMoment = extendMoment(moment);
     const { run } = this.props;
     const dateItems = [];
 
-    dateItems.push(this.getVersionDevDateItem(run));
-    dateItems.push(this.getVersionFreezeDateItem(run));
+    dateItems.push(this.getVersionDevelopmentPeriod(run));
+    dateItems.push(this.getVersionFreezePeriod(run));
 
-    let count = 2;
-    for (const devHoliday of run.devHolidays) {
-      dateItems.push(this.getDevHolidayDateItem(count, devHoliday));
-      count += 1;
+    const versionRange = exMoment.range(run.version.startDate, moment(run.version.freezeDate));
+    for (const firstWeekDay of versionRange.by('week')) {
+      const monday = moment(firstWeekDay).startOf('week');
+      const friday = moment(monday).add(4, 'day').endOf('day');
+
+      const startOfWeek = moment(monday).isAfter(run.version.startDate) ? moment(monday) : moment(run.version.startDate);
+      const endOfWeek = moment(friday).isAfter(run.version.freezeDate) ? moment(run.version.freezeDate).subtract(1, 'day').endOf('day') : moment(friday);
+
+      const weekRange = exMoment.range(startOfWeek, endOfWeek);
+      let actualDevDays = 0;
+      let totalDevDays = 0;
+      for (const day of weekRange.by('day')) {
+        for (const developer of run.developers) {
+          const timelineDay = this.buildTimelineItemAt(developer, day);
+          if (this.isNumeric(timelineDay.title)) {
+            actualDevDays += Number(timelineDay.title);
+            totalDevDays += 1;
+          }
+          dateItems.push(timelineDay);
+        }
+      }
+
+      dateItems.push(this.buildWeekItemBetween(startOfWeek, endOfWeek, actualDevDays, totalDevDays));
     }
     return dateItems;
   }
@@ -75,8 +174,8 @@ export default class HolidaysTimeline extends React.Component {
   render() {
     const { run } = this.props;
     return <Timeline groups={this.getGroups()} items={this.getDateItems()} canMove={false} canChangeGroup={false} canResize={false}
-              itemHeightRatio={1} defaultTimeStart={run.version.startDate}
-              defaultTimeEnd={run.version.freezeDate}/>;
+              itemHeightRatio={1} defaultTimeStart={moment().startOf('week')}
+              defaultTimeEnd={moment(run.version.startDate).add(2, 'week')}/>;
   }
 }
 
